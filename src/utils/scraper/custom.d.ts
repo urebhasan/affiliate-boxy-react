610a249c1ee1c697fe105a86acc69cbda38e5239
Mohsin Ali <mohsin.ali.the.great@gmail.com>
2020-12-04 13:45:58 +0500

    removed web worker

--- src/utils/scraper/custom.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'comlink-loader!*' {
-    class Scraper extends Worker {
-        constructor();
-
-        scrapeSite(hostname: string, storeDomains: { [key: string]: string[] }): Promise<void>;
-    }
-
-    export = Scraper;
-}
